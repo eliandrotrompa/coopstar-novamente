e984d1366d6cd951f7f5170fc71345d3373dcb47
ELIANDRO MATEUS DOS SANTOS <eliandro.santos@senaisp.edu.br>
2026-08-08 18:08:52 -0300

    feat: atualizar motociclista hero para versão oriental

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ const localBusinessSchema = {
   name: site.name,
   description:
     "Moto frete e delivery de documentos e pequenos volumes em São Paulo capital e Grande SP, 24 horas.",
-  image: `${site.url}/imagens/hero-loira6.png`,
+  image: `${site.url}/imagens/hero-oriental.png`,
   telephone: `+55-11-5052-3563`,
   email: site.email,
   url: site.url,
